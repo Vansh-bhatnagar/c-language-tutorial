32c0cf0b46b7454fa8f464e3a680a41e7128f670
Vansh-bhatnagar <24f2003416@ds.study.iitm.ac.in>
2025-10-30 20:50:22 +0530

    Add files via upload

---
+++ prog2.c
@@ -0,0 +1,10 @@
+// program to work with variables using printf() function
+#include <stdio.h>
+void main()
+{
+	int x=40, y=50, z=64;
+	float avg;
+	avg=(x+y+z)/3.0;
+	printf("%d, %d, %d, %.2f",x,y,x+y,avg);
+	
+}
