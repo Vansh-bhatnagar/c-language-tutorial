bad5b5e6e92d8bf55bb63516848163402a19e5a1
Vansh-bhatnagar <24f2003416@ds.study.iitm.ac.in>
2025-10-30 20:51:46 +0530

    Add files via upload

---
+++ prog3.c
@@ -0,0 +1,12 @@
+// program to work with variables using printf() and scanf() function
+#include <stdio.h>
+void main()
+{
+	int sub1, sub2, sub3, tot;
+	float avg;
+	printf("Enter 3 subject marks ");
+	scanf("%d%d%d",&sub1,&sub2,&sub3);
+	tot=sub1+sub2+sub3;
+	avg=tot/3.0;
+	printf("total is %d, average is %.2f",tot,avg);
+}
